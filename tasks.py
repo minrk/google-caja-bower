a1714e7ead88244d313a692225785352652d5303
MinRK <benjaminrk@gmail.com>
2014-02-14 15:28:59 -0800

    invoke tasks.py

---
+++ tasks.py
@@ -0,0 +1,68 @@
+"""invoke script for google-caja"""
+from contextlib import contextmanager
+import glob
+import json
+import os
+from os.path import (exists, basename)
+import shutil
+
+cd = os.chdir
+cp = shutil.copy2
+
+from invoke import run, task
+
+caja_mirror = "https://github.com/minrk/google-caja-mirror"
+upstream = "upstream"
+dest = "google-caja"
+build_version = 0
+patch_version = 0
+
+@contextmanager
+def cd(path):
+    """context manager for running in a working directory"""
+    save = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(save)
+
+@task
+def ant():
+    """build caja with ant"""
+    fetch(False)
+    cp("git-svn-revision", "{}/tools/svnversion-nocolon".format(upstream))
+    with cd(upstream):
+        run("ant jars-no-src")
+
+@task
+def fetch(update=True):
+    """fetch the actual google-caja repo into %s""" % upstream
+    if not exists(upstream):
+        run("git clone {} {} --depth 1".format(caja_mirror, upstream))
+    elif update:
+        with cd(upstream):
+            run("git pull")
+
+@task
+def version():
+    """write the svn revision to bower.json"""
+    fetch(False)
+    with cd(upstream):
+        result = run("../git-svn-revision")
+    revision = int(result.stdout)
+    vs = "%i.%i.%i" % (revision, build_version, build_version)
+    with open("bower.json") as f:
+        info = json.load(f)
+    info['version'] = vs
+    with open("bower.json", 'w') as f:
+        json.dump(info, f, sort_keys=True, indent=2)
+
+@task
+def build():
+    """build targets, and stage them into %s""" % dest
+    ant()
+    run("rm -rf {}".format(dest))
+    os.mkdir(dest)
+    for f in glob.glob("{}/ant-lib/com/google/caja/plugin/*.js".format(upstream)):
+        shutil.copy(f, "{}/{}".format(dest, basename(f)))
